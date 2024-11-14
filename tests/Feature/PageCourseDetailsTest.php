dddbb5b50ffd21d60fcfb9adbd11f63d9c229e3c
manuel medina <manuel.mv1191@gmail.com>
2024-11-14 11:10:15 -0300

    tests de la página de detalles del curso

---
+++ tests/Feature/PageCourseDetailsTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\Course;
+use App\Models\Video;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use function Pest\Laravel\get;
+
+uses(RefreshDatabase::class);
+
+it('shows course details', function () {
+    // Arrange
+    $course = Course::factory()->create([
+        'tagline' => 'Course tagline',
+        'image' => 'image.png',
+        'learnings' => [
+            'Learn laravel routes',
+            'Learn laravel views',
+            'Learn laravel commands',
+        ],
+    ]);
+
+    // Act & Assert
+    get(route('course-details', $course))
+        ->assertOk()
+        ->assertSeeText([
+            $course->title,
+            $course->description,
+            'Course tagline',
+            'Learn laravel routes',
+            'Learn laravel views',
+            'Learn laravel commands',
+        ])
+        ->assertSee('image.png');
+});
+
+it('shows course video count', function () {
+    // Arrange
+    $course = Course::factory()->create();
+    Video::factory()->count(3)->create(['course_id' => $course->id]);
+
+    // Act & Assert
+    get(route('course-details', $course))
+        ->assertOk()
+        ->assertSeeText('3 videos');
+});
