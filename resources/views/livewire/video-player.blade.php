c8d22d61b1528ea3b4467aff73e316f6c3a4a3a8
manuel medina <manuel.mv1191@gmail.com>
2024-11-28 12:11:17 -0300

    testing de video player

--- resources/views/livewire/video-player.blade.php
+++ resources/views/livewire/video-player.blade.php
@@ -3,4 +3,12 @@
     {{-- The whole world belongs to you. --}}
     <h3>{{ $video?->title }} ({{ $video->getReadableDuration() }})</h3>
     <p>{{ $video->description }}</p>
+
+    <ul>
+        @foreach ($courseVideos as $courseVideo)
+            <li>
+                <a href="{{ route('pages.course-videos', $courseVideo) }}">{{ $courseVideo->title }}</a>
+            </li>
+        @endforeach
+    </ul>
 </div>
--- tests/Feature/Livewire/VideoPlayerTest.php
+++ tests/Feature/Livewire/VideoPlayerTest.php
@@ -3,6 +3,7 @@
 use App\Livewire\VideoPlayer;
 use App\Models\Course;
 use App\Models\Video;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Livewire\Livewire;
 
 it('shows details for given video', function () {
@@ -31,4 +32,30 @@ it('shows given video', function () {
     $video = $course->videos->first();
     Livewire::test(VideoPlayer::class, ['video' => $video])
         ->assertSeeHtml('<iframe src="https://player.vimeo.com/video/'.$video->vimeo_id.'"');
+});
+
+it('shows list of all course videos', function () {
+    // Arrange
+    $course = Course::factory()
+        ->has(Video::factory()
+            ->count(3)
+            ->state(new Sequence(
+                ['title' => 'First video'],
+                ['title' => 'Second video'],
+                ['title' => 'Third video']
+            ))
+        )->create();
+
+    // Act & Assert
+    Livewire::test(VideoPlayer::class, ['video' => $course->videos()->first()])
+        ->assertSee([
+            'First video',
+            'Second video',
+            'Third video',
+        ])
+        ->assertSeeHtml([
+            route('pages.course-videos', Video::where('title', 'First video')->first()),
+            route('pages.course-videos', Video::where('title', 'Second video')->first()),
+            route('pages.course-videos', Video::where('title', 'Third video')->first()),
+        ]);
 });
