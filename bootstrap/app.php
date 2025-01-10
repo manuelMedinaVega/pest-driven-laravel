eedccb0f4ef35aa992b7e3370eab7d7e426653d4
manuel medina <manuel.mv1191@gmail.com>
2025-01-10 17:05:30 -0500

    except csrf for webhooks

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
             'paddle/*',
+            'webhooks',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
