14fef0c096a0e3b57f87accff6b1f8c861e770f8
manuel medina <manuel.mv1191@gmail.com>
2025-03-05 22:27:16 -0500

    testing coverage report 2

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,5 @@ Route::middleware([
 Route::webhooks('webhooks');
 
 Route::get('testing-actions', function() {
-    Log::info('testing, coverage report');
+    Log::info('testing, coverage report 2');
 });
